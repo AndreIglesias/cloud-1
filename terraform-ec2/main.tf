c406edf26025e60163a52e053f91acf63e0ec5f4
AndreIglesias <ciglesia@student.42.fr>
2024-06-09 12:57:02 +0200

    terraform install docker compose

--- terraform-ec2/main.tf
+++ terraform-ec2/main.tf
@@ -2,6 +2,36 @@ provider "aws" {
   region = "eu-west-3"
 }
 
+resource "aws_security_group" "wordpress_sg" {
+  name        = "wordpress_sg"
+  description = "Allow SSH and HTTPS traffic"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "wordpress_sg"
+  }
+}
+
 resource "aws_instance" "wordpress" {
   ami           = "ami-0326f9264af7e51e2" # Canonical, Ubuntu, 22.04 LTS, amd64 jammy
   instance_type = "t2.micro"
@@ -11,13 +41,26 @@ resource "aws_instance" "wordpress" {
     Name = "WordPress-Server"
   }
 
+  root_block_device {
+    volume_size = 8 # GiB
+  }
+
+  vpc_security_group_ids = [aws_security_group.wordpress_sg.id]
+
   provisioner "remote-exec" {
     inline = [
       "sudo apt-get update",
-      "sudo apt-get install -y docker.io",
-      "sudo systemctl start docker",
+      "sudo apt-get install -y ca-certificates curl",
+      "sudo install -m 0755 -d /etc/apt/keyrings",
+      "sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc",
+      "sudo chmod a+r /etc/apt/keyrings/docker.asc",
+      "echo \"deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo \"$VERSION_CODENAME\") stable\" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null",
+      "sudo apt-get update",
+      "sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin",
+      "sudo usermod -aG docker ubuntu",
+      "newgrp docker",
       "sudo systemctl enable docker",
-      "sudo usermod -aG docker ubuntu"
+      "sudo systemctl start docker"
     ]
 
     connection {
